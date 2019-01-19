ebf4fe325f53d218e5bbd28fbaee195a0b318c4a
Pete Jemian <prjemian@gmail.com>
2019-01-19 14:52:02 -0600

    wip -- on plane

--- adl_parser.py
+++ adl_parser.py
@@ -9,6 +9,7 @@ import pyRestTable
 
 
 TEST_FILE = "/usr/local/epics/synApps_5_8/support/xxx-5-8-3/xxxApp/op/adl/xxx.adl"
+TEST_FILE = "/usr/local/epics/synApps_5_8/support/motor-6-9/motorApp/op/adl/motorx_all.adl"
 
 
 class MedmBlock(object):
@@ -42,10 +43,41 @@ class MedmGeometry(object):
 class MedmWidgetBase(object):
     """contains items common to all MEDM widgets"""
     
-    def __init__(self):
+    def __init__(self, parent, block_type, *args, **kwargs):
+        self.parent = parent
+        self.medm_block_type = block_type
         self.color = None
         self.geometry = None
         self.contents = []
+
+
+class MedmGenericWidget(MedmWidgetBase): pass
+
+
+class Medm_file(MedmWidgetBase):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        parent = args[0]
+        print("token %d" % parent.tokenPos, " in file block ")
+
+
+known_handlers = {
+    # MEDM block            adl_parser class
+    "choice button":        None,
+    "color map":            None,
+    "composite":            None,
+    "display":              None,
+    "file":                 Medm_file,
+    "message button":       None,
+    "oval":                 None,
+    "polyline":             None,
+    "rectangle":            None,
+    "related display":      None,
+    "text entry":           None,
+    "text":                 None,
+    "text update":          None,
+    }
 
 
 class MEDM_Reader(object):
@@ -61,19 +93,33 @@ class MEDM_Reader(object):
         self.contents = MedmBlock("")
     
     def parse(self, level=0):
-        for token in self.tokens:
+        while self.tokenPos < self.numTokens:
+            token = self.tokens[self.tokenPos]
             token_name = self.getTokenName(token)
+
             if token_name == "OP" and token.string == "{":
                 self.brace_nesting += 1
             elif token_name == "OP" and token.string == "}":
                 self.brace_nesting -= 1
             
-            if self.brace_nesting == level and token_name in ("NAME STRING".split()):
-                # token_name == "NAME"
-                print("  "*self.brace_nesting, token_name, token.string)
-                block = MedmBlock(token.string)
-                self.contents.contents.append(block)
+            if self.brace_nesting == level:
+                if token_name in ("NAME STRING".split()):
+                    obj = known_handlers.get(token.string) or MedmGenericWidget
+                    block = obj(self, token.string)
+                    self.contents.contents.append(block)
+
+                    print(
+                        self.tokenPos, 
+                        "  "*self.brace_nesting, 
+                        token_name, 
+                        token.string)
+            
+            self.tokenPos += 1
         
+    @property
+    def numTokens(self):
+        return len(self.tokens)
+    
     def getTokenName(self, token):
         return tokenize.tok_name[token.type]
 
