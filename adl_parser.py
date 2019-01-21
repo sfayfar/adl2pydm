eef7dad511d669eca632e8e2c8f198f767f56684
Pete Jemian <prjemian@gmail.com>
2019-01-20 22:09:16 -0600

    use namedtuple and work on the parser

--- adl_parser.py
+++ adl_parser.py
@@ -3,9 +3,11 @@
 use the tokenizer to read the MEDM file
 """
 
+from collections import namedtuple
 import logging
 import token
 import tokenize
+
 
 TEST_FILE = "/usr/local/epics/synApps_5_8/support/xxx-5-8-3/xxxApp/op/adl/xxx.adl"
 TEST_FILE = "/usr/local/epics/synApps_5_8/support/motor-6-9/motorApp/op/adl/motorx_all.adl"
@@ -14,32 +16,22 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
+# a color used in MEDM
+Color = namedtuple('Color', 'r g b')
+
+# MEDM's object block contains the widget geometry
+Geometry = namedtuple('Geometry', 'x y width height')
+
+# MEDM's key = value assignment
+Assignment = namedtuple('Assignment', 'key value')
+
+
 class MedmBlock(object):
     
     def __init__(self, nm):
         self.name = nm
         self.contents = []
         self.tokens = []
-
-
-class MedmColor(object):
-    """MEDM widget color"""
-    
-    def __init__(self, r, g, b, intensity=255):
-        self.r = r
-        self.g = g
-        self.b = b
-        self.intensity = intensity
-
-
-class MedmGeometry(object):
-    """MEDM's object block contains the widget geometry"""
-    
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
 
 
 class MedmWidgetBase(object):
@@ -71,15 +63,23 @@ class Medm_file(MedmWidgetBase):
         parent = args[0]
 
 
-known_handlers = {
+widget_handlers = {
     # MEDM block            adl_parser class
+    "basic attribute":      None,
+    "children":             None,
     "choice button":        None,
     "color map":            None,
     "composite":            None,
-    "display":              None,
+    "control":              None,
+    "display":              None,       # display[n] - an oddball
+    "dynamic attribute":    None,
     "file":                 Medm_file,
+    "limits":               None,
+    "menu":                 None,
     "message button":       None,
+    "monitor":              None,
     "oval":                 None,
+    "points":               None,
     "polyline":             None,
     "rectangle":            None,
     "related display":      None,
@@ -94,6 +94,11 @@ class MEDM_Reader(object):
     read (and parse) entire MEDM .adl file
     """
     
+    block_handlers = {
+        "object": None,
+        "colors": None,
+        }
+    
     def __init__(self, filename):
         self.filename = filename
         self.tokens = self.tokenizeFile()
@@ -103,13 +108,14 @@ class MEDM_Reader(object):
         self.contents = MedmBlock("")
     
     def parse(self, owner=None, level=0):
-        owner = owner or self
+        owner = owner or self.contents
         while self.tokenPos < self.numTokens:
             token = self.tokens[self.tokenPos]
             token_name = self.getTokenName(token)
 
             if token_name == "OP":
                 if token.string == "{":
+                    logger.WARN("incrementing self.brace_nesting")
                     self.brace_nesting += 1
                 elif token.string == "}":
                     self.brace_nesting -= 1
@@ -120,13 +126,59 @@ class MEDM_Reader(object):
             
             if self.brace_nesting == level:
                 if token_name in ("NAME STRING".split()):
-                    # TODO: look ahead at next OP token:
-                    #   if "{" : make new block
-                    #   or "=" : add to owner as assignment
-                    obj = known_handlers.get(token.string) or MedmGenericWidget
-                    block = obj(self, token.string)
-                    self.contents.contents.append(block)
-                    self.print_token(token)
+                    logger.debug(("token #%d : name=%s" % (self.tokenPos, token_name)))
+                    if self.isAssignment:
+                        # handle assignment operation
+
+                        key = token.string
+                        
+                        # TODO: is it important to identify if number or string?
+                        ePos = self.getNextTokenName("NL")
+                        
+                        # tokenize splits up some things porrly
+                        # we'll parse it ourselves here
+                        pos = token.line.find("=") + 1
+                        value = token.line[pos:].strip().strip('"')
+                        
+                        assignment = Assignment(key, value)
+                        owner.contents.append(assignment)
+                        self.tokenPos = ePos
+                        logger.debug(("assignment: %s = %s" % (key, value)))
+
+                    elif self.isBlockStart:
+                        if token.string == "colors":
+                            text = ""
+                            for offset, tok in enumerate(self.tokens[self.tokenPos+2:]):
+                                if tok.string == "}" and self.getTokenName(tok) == "OP":
+                                    break
+                                else:
+                                    text += tok.string
+                            
+                            def parse_colors(rgbhex):
+                                r = int(rgbhex[:2], 16)
+                                g = int(rgbhex[2:4], 16)
+                                b = int(rgbhex[4:6], 16)
+                                return Color(r, g, b)
+                            
+                            key = token.string
+                            value = list(map(parse_colors, text.rstrip(",").split()))
+                            assignment = Assignment(token.string, value)
+                            logger.debug(("assignment: %s = %s" % (key, "length=%d" % len(value))))
+                            owner.contents.append(assignment)
+
+                            self.tokenPos += 2 + offset
+
+                        else:
+                            obj = widget_handlers.get(token.string) or MedmGenericWidget
+                            block = obj(self, token.string)
+                            logger.debug(("created %s(name=\"%s\")" % (block.__class__.__name__, token.string)))
+                            self.brace_nesting += 1
+                            owner.contents.append(block)
+                        self.tokenPos += 1
+
+                    else:
+                        self.print_token(token)
+
             elif self.brace_nesting > level:
                 logger.debug(("enter level %d" % self.brace_nesting))
                 self.tokenPos += 1
@@ -145,17 +197,55 @@ class MEDM_Reader(object):
             token_name, 
             token.string.rstrip()))
         
+    def getTokenSequence(self, start=None, length=2):
+        start = start or self.tokenPos
+        if start+length >= self.numTokens:
+            return []
+        return self.tokens[start:start+length]
+        
     @property
     def numTokens(self):
         return len(self.tokens)
     
+    @property
+    def isAssignment(self):
+        """are we looking at a key=value assignment?"""
+        tokens = self.getTokenSequence(length=2)
+        if len(tokens) == 2:
+            tt_seq = " ".join(map(self.getTokenName, tokens))
+            if tt_seq in ("NAME OP", "STRING OP"):
+                return tokens[-1].string.rstrip() == "="
+        return False
+    
+    @property
+    def isBlockStart(self):
+        """are we looking at an MEDM block starting?:  name {"""
+        tokens = self.getTokenSequence(length=2)
+        if len(tokens) == 2:
+            tt_seq = " ".join(map(self.getTokenName, tokens))
+            if tt_seq in ("NAME OP", "STRING OP"):
+                return tokens[-1].string.rstrip() == "{"
+        return False
+
     def getTokenName(self, token):
+        """return the name of this token"""
         return tokenize.tok_name[token.type]
 
+    def getNextTokenName(self, token_name):
+        """return the index of the next token with given name(s)"""
+        # make sure token_name is a list
+        if not isinstance(token_name, (set, tuple, list)):
+            token_name = [token_name]
+        
+        # look through the remaining tokens for the next occurence
+        for offset, token in enumerate(self.tokens[self.tokenPos:]):
+            if self.getTokenName(token) in token_name:
+                return self.tokenPos + offset       # - 1
+        
+        raise ValueError("unexpected: failed to find next named token")
+
     def tokenizeFile(self):
-        '''
-        tokenize just one file
-        '''
+        """tokenize just one file"""
         with open(self.filename, "rb") as f:
             tokens = tokenize.tokenize(f.readline)
             return list(tokens)
@@ -165,5 +255,4 @@ if __name__ == "__main__":
     reader = MEDM_Reader(TEST_FILE)
     reader.parse()
     ttypes = [reader.getTokenName(token) for token in reader.tokens]
-    print(" ".join(ttypes))
     print("done")
