192e410a555128a33c6b10ea6158b3d0bc9b1178
Pete R. Jemian <prjemian@gmail.com>
2019-02-07 08:26:42 -0600

    generalize

--- env_setup.sh
+++ env_setup.sh
@@ -3,7 +3,7 @@
 #   . env_setup.sh
 
 conda activate pydm
-export PYQTDESIGNERPATH=/home/prjemian/Documents/eclipse/adl2pydm/pyqtdesignerpath:
+export PYQTDESIGNERPATH=${HOME}/Documents/eclipse/adl2pydm/pyqtdesignerpath:
 
 # the  PYQTDESIGNERPATH directory contains this file:  pydm_designer_plugin.py 
 # and that file contains this one line:
