41e3f595c1b22322c687003aef48091bc5fe5784
Pete Jemian <prjemian@gmail.com>
2019-10-26 13:30:23 -0500

    ENH #22  basic setup

---
+++ setup.py
@@ -0,0 +1,41 @@
+"""
+packaging setup for apstools
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+import sys
+import versioneer
+
+here = path.abspath(path.dirname(__file__))
+sys.path.insert(0, path.join('src',))
+import adl2pydm as package
+
+
+__entry_points__  = {
+    'console_scripts': [
+        'adl2pydm = adl2pydm.adl2pydm:main',
+        ],
+    #'gui_scripts': [],
+}
+
+
+setup(
+    author           = package.__author__,
+    author_email     = package.__author_email__,
+    classifiers      = package.__classifiers__,
+    description      = package.__description__,
+    entry_points     = __entry_points__,
+    license          = package.__license__,
+    long_description = package.__long_description__,
+    install_requires = package.__install_requires__,
+    name             = package.__project__,
+    #platforms        = package.__platforms__,
+    packages         = find_packages(exclude=package.__exclude_project_dirs__),
+    url              = package.__url__,
+    version          = versioneer.get_version(),
+    cmdclass         = versioneer.get_cmdclass(),
+    zip_safe         = package.__zip_safe__,
+    python_requires  = package.__python_version_required__,
+ )
