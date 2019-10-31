13544f4237eed1746406269b9fa266535d9c119f
Pete Jemian <prjemian@gmail.com>
2019-10-30 23:30:07 -0500

    ENH #16 menu

--- src/adl2pydm/output_handler.py
+++ src/adl2pydm/output_handler.py
@@ -285,6 +285,7 @@ class Widget2Pydm(object):      # TODO: move to output_handler module
         pv = self.get_channel(block.contents["control"])
         self.write_channel(qw, pv)
         self.write_tooltip(qw, pv)
+        self.write_colors_style(qw, block)
         
     def write_block_message_button(self, parent, block, nm, qw):
         pv = self.get_channel(block.contents["control"])
