e815538133674282b6d175d3493e5b6fb886c762
Pete Jemian <prjemian@gmail.com>
2019-11-18 12:32:05 -0600

    fixes #38

--- src/adl2pydm/output_handler.py
+++ src/adl2pydm/output_handler.py
@@ -621,9 +621,10 @@ class Widget2Pydm(object):
         self.write_direction(qw, block)
         self.write_limits(qw, block)
         
-        precision = block.contents.get("dPrecision")
-        if precision is not None:
-            self.writer.writeProperty(qw, "precision", str(precision), tag="number")
+        # TODO: https://github.com/BCDA-APS/adl2pydm/issues/37
+        # precision = block.contents.get("dPrecision")
+        # if precision is not None:
+        #     self.writer.writeProperty(qw, "precision", str(precision), tag="number")
 
     def write_block_wheel_switch(self, parent, block, nm, qw):
         pv = self.get_channel(block.contents["control"])
