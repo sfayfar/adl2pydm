00fcff8632b6010a1cb4d8d791477314ab6296ab
Pete R Jemian <prjemian@gmail.com>
2021-10-25 12:42:24 -0500

    TST #74

--- starter.py
+++ starter.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
+"""
+Used by developer to debug.
+"""
+
 import os
 import sys
 
 _path = os.path.dirname(__file__)
 sys.path.append(os.path.join(_path, "."))
 from adl2pydm import cli
+
+sys.argv.append(
+    os.path.join(
+        os.path.dirname(__file__),
+        "adl2pydm",
+        "tests",
+        "medm",
+        "testDisplay.adl"
+    )
+)
 
 cli.main()
